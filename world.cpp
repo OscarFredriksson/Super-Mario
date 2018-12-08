be5492ef9a6ad00e8cecfed654aa496c228f2be4
OscarFredriksson <osfr1701@student.miun.se>
2018-12-08 02:37:31 +0100

    Kan nu läsa in en bana från en fil

--- world.cpp
+++ world.cpp
@@ -1,15 +1,44 @@
 #include "world.h"
 #include "Blocks/ground_block.h"
 #include "Blocks/brick_block.h"
+#include "Blocks/ground_block.h"
+#include "fstream"
+#include "iostream"
+#include "string"
 
 World::World()
 {
     fillMap();
 
-    map[1][11] = new Brick_Block(1, 11);
-    map[2][12] = new Ground_Block(2, 12);
-    map[3][12] = new Ground_Block(3, 12);
+    std::ifstream file;
+    file.open("map.txt");
+    if(!file.is_open())
+    {
+        std::cerr << "Kunde inte öppna filen..." << std::endl;
+        return;
+    }
+    for(int i = 0; !file.eof(); i++)
+    {   
+        std::string str;
+        file >> str;
 
+        std::cout << str << std::endl;
+
+        for(int j = 0; j < str.length(); j++)
+        {
+            if(str[j] == '1')   map[j][i] = new Brick_Block(j, i);
+            //if(str[j] == '2')   map[j][i] = new Question_Block(j, i);
+            if(str[j] == '4') map[j][i] = new Ground_Block(j, i);
+        }   
+    }
+    file.close();
+
+
+    //map[1][11] = new Brick_Block(1, 11);
+    //map[2][12] = new Ground_Block(2, 12);
+    //map[3][12] = new Ground_Block(3, 12);
+
+    
 
 }
 
